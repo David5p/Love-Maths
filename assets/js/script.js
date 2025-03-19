3460b670efd1feb461b8ed16391d395462ddbc4b
David5p <davidarcherqaz@gmail.com>
2025-03-19 13:26:25 +0100

    change alert to checkAnswer function event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+               checkAnswer ();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -45,7 +45,7 @@ function checkAnswer() {
 function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById("operator").innerText);
+    let operator = document.getElementById("operator").innerText;
     if (operator==="+") {
         return[operand1+operand2, "addition"];
     } else {
