d9b40c90b29d515f7440c2a94681900c9418ed5e
David5p <davidarcherqaz@gmail.com>
2025-03-19 09:38:56 +0100

    Add code to generate two random numbers for game play

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-//Wait for the DOM to finish loading before running the game
-//Get the button elements and add event listeners to them 
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
-let buttons= document.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
 
-    for (let button on buttons) {
+    for (let button of buttons) {
         button.addEventListener("click", function() {
-            if(this.getAttribute("data-type")==="submit") {
-            alert("You clicked Submit!");
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
             } else {
-            let gameType= this.getAttribute("data-type");
-            alert(`You clicked ${gameType}`);
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
             }
-        })
+        });
     }
-})
+});
 
 function runGame() {
 
@@ -45,8 +45,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-
-}
-function displayDivisionQuestion() {
-
+    
 }
